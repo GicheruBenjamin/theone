ae47533c1332d9dbf29deaee0f0c19baed0249a6
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2026-01-16 19:39:43 +0300

    feature: Move the api interaction to api.js away from services.js ...

--- App.js
+++ App.js
@@ -1,18 +1,7 @@
 // App.js
-import {
-    renderHomepage,
-    renderAboutpage,
-    renderLoginpage,
-    renderHomelayout,
-} from "./Components.js";
+
 
 const app = document.getElementById("app");
-console.log(location.pathname);
 
-function createApp(app) {
-    // home layout
-    const homelayout = renderHomelayout();
-    app.appendChild(homelayout);
-}
 
-createApp(app);
+
