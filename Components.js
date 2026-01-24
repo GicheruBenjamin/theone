18dd65b5da598451613f0f3ca34b199b3f95771c
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2026-01-24 22:16:57 +0300

    feature: Not working but in the process of fixing.

--- Components.js
+++ Components.js
@@ -52,50 +52,7 @@ export function Component(tag, props = {}) {
 }
 
 
-function renderLoginform(){
-    let Loginform = Component("form",{
-        className : "loginform",
-        children : [
-            Component("label",{
-                text : "Username",
-                className : "loginformlabel",
-                attributes : {
-                    for : "username"
-                }
-            }),
-            Component("input",{
-                className : "loginforminput",
-                attributes : {
-                    type : "text",
-                    name : "username",
-                    placeholder : "Username"
-                }
-            }),
-            Component("label",{
-                text : "Email",
-                className : "loginformlabel",
-                attributes : {
-                    for : "email"
-                }
-            }),
-            Component("input",{
-                className : "loginforminput",
-                attributes : {
-                    type : "email",
-                    name : "email",
-                    placeholder : "Email"
-                }
-            }),
-            Component("button",{
-                text : "Login",
-                className : "loginformbutton",
-                attributes : {
-                    type : "submit"
-                }
-            })
-        ]})
-        return Loginform
-}
+
 
 
 
