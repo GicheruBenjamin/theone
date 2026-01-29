9c0c81ced3cd905e690cd0945e6f9602c63fea4f
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2026-01-29 21:29:00 +0300

    feature: Deviated to get photos i.e make things interesting..

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
 // App.js
-
-import getApiData from "./Api.js"
-import { Dashboardoverview } from "./Dashboardcomponents.js";
+import  getPhotoUrl  from "./Photos.js";
 import Component from "./Components.js";
 
 const app = document.getElementById("app");
 
-function renderUsersview(app){
-    // Get the user data
-    const usersres = getApiData("users")
-    if(!usersres.ok){
-        const error = Component("div",{
-            className : "error",
-            text : usersres.message
-        })
-        app.appendChild(error)
-    }
-    else{
-        const user = usersres.data[0]
-        const Dashboard = Dashboardoverview(user)
-        app.appendChild(Dashboard)
-    }
+function renderImg(url, alt, width, height) {
+    return Component("img", {
+        className: "img-fluid",
+        attributes: {
+            src: url,
+            alt,
+            width,
+            height
+        }
+    });
 }
 
-renderUsersview(app)
+function renderImages(container) {
+    const dimensions = [
+        { width: 100, height: 100 },
+        { width: 200, height: 200 },
+        { width: 300, height: 300 },
+        { width: 400, height: 400 },
+        { width: 500, height: 500 },
+        { width: 600, height: 600 }
+    ];
+
+    dimensions.forEach(({ width, height }, index) => {
+        const res = getPhotoUrl(width, height, index);
+        if (!res.ok) return;
+
+        const img = renderImg(
+            res.data,
+            `Random image ${width}x${height}`,
+            width,
+            height
+        );
+
+        container.appendChild(img);
+    });
+}
+
+renderImages(app);
